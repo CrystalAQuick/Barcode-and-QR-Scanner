e04cff4260a9d1f7b8c4175b07d2517a6b5e6623
Crystal Quick <cacquick@gmail.com>
2020-10-28 08:00:19 -0500

    adding more comments throughout code

--- app/src/main/java/com/example/quickdemozxing/CapturedActivityInformation.java
+++ app/src/main/java/com/example/quickdemozxing/CapturedActivityInformation.java
@@ -7,5 +7,6 @@ import androidx.appcompat.app.AlertDialog;
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class CapturedActivityInformation extends CaptureActivity {
-
+    // This does exactly what the CaptureActivity does,
+    // the only difference is the orientation settings. (Manifest file)
 }
