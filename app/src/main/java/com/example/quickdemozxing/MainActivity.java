bc119443c5d4b531a93ecf88429c30d4c8cb7f0a
Crystal Quick <cacquick@gmail.com>
2020-10-27 21:42:21 -0500

    adding logging to project

--- app/src/main/java/com/example/quickdemozxing/MainActivity.java
+++ app/src/main/java/com/example/quickdemozxing/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,21 +25,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnScan = findViewById(R.id.btnScan);
         btnScan.setOnClickListener(this);
+        Log.d("CQ - TESTING", "onCreate");
     }
 
     @Override
     public void onClick(View v) {
         scanItemCode();
+        Log.d("CQ - TESTING", "onClick");
     }
 
     private void scanItemCode(){
+        // IntentIntegrator is a utility class that allows integration using Intents
 
+        // Initiating an instance of IntentIntegrator
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.setCaptureActivity(InformationReturnedFromScan.class);
         intentIntegrator.setOrientationLocked(false);
+
+        // This will allow almost all barcode formats to be scanned.
         intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+
+        // This is the text that appears when you are trying to scan an item
         intentIntegrator.setPrompt("Scanning item");
+
+        // calling initiateScan
         intentIntegrator.initiateScan();
+        Log.d("CQ - TESTING", "scanItemCode");
     }
 
     @Override
@@ -53,23 +65,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         scanItemCode();
+                        Log.d("CQ - TESTING", "onActivityResult - SCAN AGAIN");
                     }
                 }).setNegativeButton("Exit?", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         finish();
+                        Log.d("CQ - TESTING", "onActivityResult - EXIT");
+
                     }
                 });
 
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();
+                Log.d("CQ - TESTING", "Item was scanned.");
 
             } else {
                 Toast.makeText(this, "Please try again.",  Toast.LENGTH_LONG).show();
+                Log.d("CQ - TESTING", "Didnt work");
             }
 
         } else {
             super.onActivityResult(requestCode, resultCode, data);
+            Log.d("CQ - TESTING", "onActivityResult");
         }
     }
 }
